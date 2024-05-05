d9ee66a5c5f8c8849e63daaf32ae231c2e3a99de
Navit Sokolovsky <168529121+navitsoko@users.noreply.github.com>
2024-05-05 20:23:41 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import Extension, setup
+module=Extension("mysymnmfsp", sources=["symnmfmodule.c", "symnmf.c"])
+setup(name="mysymnmfsp",
+      version="1.0",
+      description="Python wrapper for custom C extension",
+      ext_modules=[module]
+      )
